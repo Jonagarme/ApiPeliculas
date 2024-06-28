ea5844825c7d04675562c4523f0020c3e3fc5cf1
Jonathan Garcia <garciajonathan268@gmail.com>
2024-06-27 21:12:48 -0500

    Agregar archivos de proyecto.

---
+++ ApiPeliculas/Modelos/Dtos/CrearCategoriaDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiPeliculas.Modelos.Dtos
+{
+    public class CrearCategoriaDto
+    {
+        //Esta validacion es importante si no se crea vacio el nombre de la categoria
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [MaxLength(60, ErrorMessage = "El número maximo de caracteres es de 100")]
+        public string Nombre { get; set; }
+    }
+}
---
+++ ApiPeliculas/Respositorio/IRepositorio/ICategoriaRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using ApiPeliculas.Modelos;
+
+namespace ApiPeliculas.Respositorio.IRepositorio
+{
+    public interface ICategoriaRepositorio
+    {
+        ICollection<Categoria> GetCategorias();
+        Categoria GetCategoria(int categoriaId);
+        bool ExisteCategoria(string nombre);
+        bool ExisteCategoria(int id);
+        bool CrearCategoria(Categoria categoria);
+        bool ActualizarCategoria(Categoria categoria);
+        bool BorrarCategoria(Categoria categoria);
+        bool Guardar();
+    }
+}
